4f6a04317faef79ab73d119121a68ea346f4d484
Peter Phanouvong <peterphanouvong@gmail.com>
2022-04-19 12:00:45 +1000

    inti

--- __tests__/first.test.jsx
+++ __tests__/first.test.jsx
@@ -1,8 +1,13 @@
 import React, {useContext} from 'react';
-import {renderHook} from '@testing-library/react-hooks';
+import {renderHook, act} from '@testing-library/react-hooks';
 import {renderKindeProvider} from './renderKindeProvider.jsx';
 import {KindeContext} from '../src/state/KindeContext';
+import {LocalStorageMock} from '../src/mocks/mock-local-storage.js';
 import * as createKindeClient from '@kinde-oss/kinde-auth-pkce-js';
+
+// eslint-disable-next-line no-undef
+global.localStorage = new LocalStorageMock();
+global.sessionStorage = new LocalStorageMock();
 
 const createKindeClientSpy = jest.spyOn(createKindeClient, 'default');
 
@@ -41,5 +46,92 @@ describe('KindeProvider', () => {
     );
 
     await waitForNextUpdate();
+  });
+
+  it('should check session when logged out', async () => {
+    const wrapper = renderKindeProvider();
+    const {waitForNextUpdate, result} = renderHook(
+      () => useContext(KindeContext),
+      {wrapper}
+    );
+    expect(result.current.isLoading).toBe(true);
+    await waitForNextUpdate();
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.isAuthenticated).toBe(false);
+  });
+
+  it('should check session when logged in', async () => {
+    const wrapper = renderKindeProvider();
+
+    localStorage.setItem(
+      'kinde_token',
+      JSON.stringify({
+        access_token: 'access_token'
+      })
+    );
+
+    const {waitForNextUpdate, result} = renderHook(
+      () => useContext(KindeContext),
+      {wrapper}
+    );
+
+    expect(result.current.isLoading).toBe(true);
+    await waitForNextUpdate();
+    expect(result.current.isLoading).toBe(true);
+    await waitForNextUpdate();
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.isAuthenticated).toBe(true);
+  });
+
+  it('should handle redirect callback success and clear the url', async () => {
+    window.history.pushState(
+      {},
+      document.title,
+      '/?code=__test_code__&state=__test_state__'
+    );
+
+    expect(window.location.href).toBe(
+      'https://www.example.com/?code=__test_code__&state=__test_state__'
+    );
+
+    sessionStorage.setItem(
+      'pkce-code-verifier-__test_state__',
+      '__test_code_verifier__'
+    );
+
+    const wrapper = renderKindeProvider();
+    const {waitForNextUpdate} = renderHook(() => useContext(KindeContext), {
+      wrapper
+    });
+
+    await waitForNextUpdate();
+    await waitForNextUpdate();
+    expect(window.location.href).toBe('https://www.example.com/');
+  });
+
+  it('should handle redirect callback success and return to app state param', async () => {
+    window.history.pushState(
+      {},
+      document.title,
+      '/?code=__test_code__&state=__test_state__'
+    );
+
+    expect(window.location.href).toBe(
+      'https://www.example.com/?code=__test_code__&state=__test_state__'
+    );
+
+    sessionStorage.setItem(
+      'pkce-code-verifier-__test_state__',
+      '__test_code_verifier__'
+    );
+
+    const wrapper = renderKindeProvider();
+    const {waitForNextUpdate} = renderHook(() => useContext(KindeContext), {
+      wrapper
+    });
+
+    await waitForNextUpdate();
+    await waitForNextUpdate();
+    expect(window.location.href).toBe('https://www.example.com/');
   });
 });
