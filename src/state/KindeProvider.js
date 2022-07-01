8d9e3f07c5531d0fdf4d99dc35daffbcaeb9f544
Peter Phanouvong <peterphanouvong@gmail.com>
2022-07-01 10:12:11 +1000

    include org stuff

--- src/state/KindeProvider.js
+++ src/state/KindeProvider.js
@@ -64,11 +64,16 @@ const KindeProvider = ({
     return () => (isSubscribed = false);
   }, [client, onRedirectCallback]);
 
-  const login = useCallback(() => client.login(), [client]);
+  const login = useCallback((options) => client.login(options), [client]);
 
-  const register = useCallback(() => client.register(), [client]);
+  const register = useCallback((options) => client.register(options), [client]);
 
-  const logout = useCallback(() => client.logout(), [client]);
+  const logout = useCallback((options) => client.logout(options), [client]);
+
+  const createOrg = useCallback(
+    (options) => client.createOrg(options),
+    [client]
+  );
 
   const getToken = useCallback(async () => {
     let token;
@@ -105,9 +110,18 @@ const KindeProvider = ({
       login,
       register,
       logout,
-      handleRedirectCallback
+      handleRedirectCallback,
+      createOrg
     };
-  }, [state, getToken, login, register, logout, handleRedirectCallback]);
+  }, [
+    state,
+    getToken,
+    login,
+    register,
+    logout,
+    handleRedirectCallback,
+    createOrg
+  ]);
 
   return (
     <KindeContext.Provider value={contextValue}>
