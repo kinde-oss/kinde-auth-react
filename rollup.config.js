1ee66ae01a359dd871cad7a7d3175c7c9c421eab
Dave Berner <davidajberner@gmail.com>
2022-12-01 00:33:59 +1100

    add types improve bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,35 @@
-import {terser} from 'rollup-plugin-terser';
-import pkg from './package.json';
+import {createRequire} from 'node:module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+import terser from '@rollup/plugin-terser';
 import babel from '@rollup/plugin-babel';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.js',
-  plugins: [
-    babel({
-      babelHelpers: 'runtime'
-    }),
-    terser()
-  ],
-  output: [
-    {
-      name: 'createKindeClient',
-      file: pkg.browser,
-      format: 'umd'
-    },
-    {
-      file: pkg.module,
-      format: 'es'
-    }
-  ],
-  external: ['react', 'react-dom']
-};
+export default [
+  {
+    input: 'src/index.js',
+    plugins: [
+      babel({
+        babelHelpers: 'runtime'
+      })
+    ],
+    output: [
+      {
+        name: 'createKindeClient',
+        file: pkg.main,
+        format: 'umd',
+        plugins: [terser()]
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
+    external: ['react', 'react-dom']
+  },
+  {
+    input: 'index.d.ts',
+    output: [{file: 'dist/index.d.ts', format: 'es'}],
+    plugins: [dts()]
+  }
+];
