7115b98ec9bb79d998ce6f115f6194ab68ba688f
Peter Phanouvong <peterphanouvong@gmail.com>
2024-10-16 15:16:21 +1100

    fix: bug in authURL

--- src/components/LoginLink.tsx
+++ src/components/LoginLink.tsx
@@ -1,7 +1,12 @@
 import React, { useMemo, useEffect, useState } from "react";
 
 // Assuming these are correctly imported from your project
-import { LoginOptions, IssuerRouteTypes, generateAuthUrl, LoginMethodParams } from '@kinde/js-utils'; 
+import {
+  LoginOptions,
+  IssuerRouteTypes,
+  generateAuthUrl,
+  LoginMethodParams,
+} from "@kinde/js-utils";
 import { useKindeAuth } from "../hooks/useKindeAuth";
 import { LocalKeys } from "../state/KindeProvider";
 
@@ -11,33 +16,39 @@ interface Props {
 }
 
 // export function LoginLink({ children, ...props }: Props) {
-  export function LoginLink({ children, authParams }: Props) {
+export function LoginLink({ children, authParams }: Props) {
   const auth = useKindeAuth();
 
   const [authUrl, setAuthUrl] = useState<string | null>(null);
+  console.log(authUrl);
 
   const authUrlPromise = useMemo(() => {
-
     const getAuthUrl = async () => {
       const authProps: LoginOptions = {
-        audience: await auth.store.getSessionItem(LocalKeys.audience) as string, 
-        clientId: await auth.store.getSessionItem(LocalKeys.clientId) as string,
-        redirectURL: '',
-        prompt: 'login',
+        audience: (await auth.store.getSessionItem(
+          LocalKeys.audience
+        )) as string,
+        clientId: (await auth.store.getSessionItem(
+          LocalKeys.clientId
+        )) as string,
+        redirectURL: "",
+        prompt: "login",
         ...authParams,
       };
-      const domain = await auth.store.getSessionItem(LocalKeys.domain) as string;
+      const domain = (await auth.store.getSessionItem(
+        LocalKeys.domain
+      )) as string;
       return generateAuthUrl(domain, IssuerRouteTypes.login, authProps);
     };
-    return getAuthUrl(); 
+    return getAuthUrl();
   }, [auth, authParams]);
 
   useEffect(() => {
     let isMounted = true; // Track if the component is still mounted
 
-    authUrlPromise.then(url => {
+    authUrlPromise.then((url) => {
       if (isMounted) {
-        setAuthUrl(url.toString());
+        setAuthUrl(url.url.toString());
       }
     });
 
@@ -47,11 +58,5 @@ interface Props {
   }, [authUrlPromise]); // Run effect whenever authUrlPromise changes
 
   // Conditionally render the link to avoid errors
-  return (
-    authUrl ? (
-      <a href={authUrl}>{children}</a> 
-    ) : (
-      <></>
-    )
-  );
+  return authUrl ? <a href={authUrl}>{children}</a> : <></>;
 }
