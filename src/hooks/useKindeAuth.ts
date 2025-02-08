d7c3b4088ee358969e6ad0558d9874fc034a9663
Daniel Rivers <me@danielrivers.com>
2025-02-08 21:57:08 +0000

    chore: cleanup

--- src/hooks/useKindeAuth.ts
+++ src/hooks/useKindeAuth.ts
@@ -4,7 +4,6 @@ import { KindeContext, KindeContextProps } from "../state/KindeContext";
 export const useKindeAuth = (): KindeContextProps => {
   const context = useContext(KindeContext);
 
-  console.log(context);
   if (context === undefined) {
     throw new Error("Oooops! useKindeAuth must be used within a KindeProvider");
   }
--- src/utils/getRedirectUrl.ts
+++ src/utils/getRedirectUrl.ts
@@ -1,10 +1,3 @@
 export const getRedirectUrl = (suppliedUrl?: string) => {
-  console.log("suppliedUrl", suppliedUrl);
-  console.log(
-    "VITE_KINDE_REDIRECT_URL",
-    import.meta.env.VITE_KINDE_REDIRECT_URL,
-  );
-  console.log("window.location.origin", window.location.origin);
-
   return suppliedUrl || window.location.origin;
 };
