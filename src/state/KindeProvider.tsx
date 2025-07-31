294e105cfd0e1f186267ea657fdba3faea512eac
Daniel Rivers <me@danielrivers.com>
2025-07-31 13:02:23 +0100

    fix: getClaim params

--- src/state/KindeProvider.tsx
+++ src/state/KindeProvider.tsx
@@ -379,8 +379,9 @@ export const KindeProvider = ({
 
       getClaim: async <T, V = string | number | string[]>(
         keyName: keyof T,
+        tokenType?: "accessToken" | "idToken"
       ): Promise<{ name: keyof T; value: V } | null> => {
-        return getClaim<T, V>(keyName);
+        return getClaim<T, V>(keyName, tokenType);
       },
       getClaims: async <T = undefined,>(
         ...args: Parameters<typeof getClaims>
